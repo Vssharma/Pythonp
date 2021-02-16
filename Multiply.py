aedf40b2152b5c593c40fa480c26b29fe4415a89
vssharma <vssharma098@gmail>
2021-02-17 00:15:23 +0530

    17'Feb'21

--- Multiply.py
+++ Multiply.py
@@ -1,13 +1,2 @@
-def multiply(a):
-	r=1
-	for v in a:
-		r*= v;
-	return r;
-
-
-list1= [2,3,6,7,3,8,9,1,3,4]
-b=multiply(list1);
-
-print (b);
-
-
+a=1
+b=2;
---
+++ Nlargetestno.py
@@ -0,0 +1,5 @@
+list1=[1,4,6,8,9,12,10];
+n=int(input("Enter the value of n:"));
+list1.sort();
+print(list1);
+print(list1[-n:2]);
